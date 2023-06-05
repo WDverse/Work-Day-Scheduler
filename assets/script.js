d2dbe34db84b79482d9e4cba3ad228f7b197118e
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-06-05 05:21:19 -0400

    added click event to save button in js file

--- assets/script.js
+++ assets/script.js
@@ -9,6 +9,19 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
+//-------------My Code----------------
+  // var saveBtn = $('.saveBtn');
+  // var eventTxt = 
+  // saveBtn.on('click', function(){
+  //   var userEvent = {
+  //     userInput: 
+
+  //   }
+  // })
+//-------------My Code----------------
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
